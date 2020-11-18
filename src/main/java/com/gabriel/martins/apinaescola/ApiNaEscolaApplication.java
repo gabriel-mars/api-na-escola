22a12f0d1f535619ca0c3928a441ac648c7db800
Gabriel Martins <gabriel.martins@b2ml.com.br>
2020-11-18 08:30:42 -0300

    Estrutura inicial do Projeto

---
+++ src/main/java/com/gabriel/martins/apinaescola/ApiNaEscolaApplication.java
@@ -0,0 +1,13 @@
+package com.gabriel.martins.apinaescola;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiNaEscolaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiNaEscolaApplication.class, args);
+	}
+
+}
